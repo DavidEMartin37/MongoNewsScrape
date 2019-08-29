d85a2e90c74fda064e995149cceed7381bb1d27e
David Martin <demartins37@yahoo.com>
2019-08-28 22:16:45 -0400

    updated scrape and clear buttons with jquery

--- public/app.js
+++ public/app.js
@@ -1,12 +1,19 @@
-$(document).ready(function() {
-  $(document).on("click", ".scrape-new", function() {
-    // event.preventDefault();
-    $.ajax({
-      method: "GET",
-      url: "http://localhost:3000/api/scrape"
-    }).then(function(articles) {
-      console.log(articles);  
-      return articles;
-      })
-  });
+$(".scrape-new").click(function() {
+  $.ajax({
+    method: "GET",
+    url: "/api/scrape"
+  }).then(function(articles) {
+    console.log(articles);  
+    location.reload(true);
+    })
+});
+
+$(".clear").click(function() {
+  $.ajax({
+    method: "GET",
+    url: "/api/clear"
+  }).then(function(articles) {
+    console.log(articles);  
+    location.reload(true);
+    })
 });
